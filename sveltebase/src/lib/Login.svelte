e4df194e2ec836e7cae26cb05002e7d1dd65f720
snorresovold <snorresovold@gmail.com>
2023-01-21 17:29:22 +0100

    Update Login.svelte

--- sveltebase/src/lib/Login.svelte
+++ sveltebase/src/lib/Login.svelte
@@ -36,24 +36,59 @@
         <button on:click={signOut}>Sign Out</button>
     </p>
     {:else}
-    <form on:submit|preventDefault>
-        <input
-        placeholder="Username"
-        type="text"
-        bind:value={username}
-        />
-    
-        <input 
-        placeholder="Password" 
-        type="password" 
-        bind:value={password} 
-        />
-        <input 
-        placeholder="PasswordConfirm" 
-        type="passwordConfirm" 
-        bind:value={passwordConfirm} 
-        />
-        <button on:click={signUp}>Sign Up</button>
-        <button on:click={login}>Login</button>
-    </form>
+    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+            <div
+                class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+            </div>
+            <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                <div class="max-w-md mx-auto">
+                    <div>
+                        <h1 class="text-2xl font-semibold">Login Form with Floating Labels</h1>
+                    </div>
+                    <form on:submit|preventDefault>
+                    <div class="divide-y divide-gray-200">
+                        <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                            <div class="relative">
+                                <input
+                                bind:value={username}
+                                autocomplete="off" 
+                                id="username" 
+                                name="username" 
+                                type="text" 
+                                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" 
+                                placeholder="username" />
+                                <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
+                            </div>
+                            <div class="relative">
+                                <input
+                                bind:value={password}
+                                id="password" 
+                                name="password" 
+                                type="password" 
+                                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                                <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                            </div>
+                            <div class="relative">
+                                <input
+                                bind:value={passwordConfirm}
+                                id="passwordConfirm" 
+                                name="passwordConfirm" 
+                                type="password" 
+                                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+                                <label for="passwordConfirm" 
+                                class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Confirm Password</label>
+                            </div>
+                            <div class="relative">
+                                <button 
+                                class="bg-blue-500 text-white rounded-md px-2 py-1"
+                                on:click={signUp}>Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     {/if}
