ca673833da863fc55557eb3e31bcb282cac5f3a4
snorresovold <snorresovold@gmail.com>
2023-01-26 18:59:52 +0100

    Update Product.svelte

--- sveltebase/src/lib/Product.svelte
+++ sveltebase/src/lib/Product.svelte
@@ -6,20 +6,20 @@
     export let desc: any;
 </script>
 <a href={"products/" + link} class="block group border-4 border-black">
-    <img
-        class="avatar"
-        src={`https://avatars.dicebear.com/api/identicon/${user}.svg`}
-        alt="avatar"
-        width="40px"
-    />
-  
-    <div class="mt-3">
-      <h3
-        class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
-      >
-        {name}
-      </h3>
-      <p>{desc}</p>
-      <p class="mt-1 text-sm text-gray-700">{price}</p>
-    </div>
-  </a>
+  <img
+      class="avatar"
+      src={`https://avatars.dicebear.com/api/identicon/${user}.svg`}
+      alt="avatar"
+      width="40px"
+  />
+
+  <div class="mt-3">
+    <h3
+      class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
+    >
+      {name}
+    </h3>
+    <p>{desc}</p>
+    <p class="mt-1 text-sm text-gray-700">{price}</p>
+  </div>
+</a>
