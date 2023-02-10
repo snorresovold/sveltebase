81029276959150be269ead2fd80313eb0f25e3d4
snorresovold <snorresovold@gmail.com>
2023-02-10 09:17:46 +0100

    Update pocketbase.ts

--- sveltebase/src/lib/pocketbase.ts
+++ sveltebase/src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('http://127.0.0.1:8090'); 
+export const pb = new PocketBase('https://scarce-salesmen.pockethost.io'); 
 
 export const currentUser = writable(pb.authStore.model);
 
