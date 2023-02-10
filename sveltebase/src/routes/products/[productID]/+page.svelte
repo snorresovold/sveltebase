1bdc50c10f4cd78e426780ce5c6671b3572dc52e
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2023-02-10 00:36:53 -0800

    Update +page.svelte

--- sveltebase/src/routes/products/[productID]/+page.svelte
+++ sveltebase/src/routes/products/[productID]/+page.svelte
@@ -10,11 +10,3 @@
 <p>{product.desc}</p>
 <p>{product.expand?.user?.username}</p>
 <UserIcon id={product.expand?.user?.id} />
-{#each product.expand.image as x}
-	<img
-		class="avatar"
-		src={x.url}
-		alt="avatar"
-		width="40px"
-	/>
-{/each}
