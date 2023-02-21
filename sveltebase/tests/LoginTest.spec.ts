95d567c86e0673a23393d03a87fcb0221c41e781
snorresovold <snorresovold@gmail.com>
2023-02-21 20:06:05 +0100

    Update LoginTest.spec.ts

--- sveltebase/tests/LoginTest.spec.ts
+++ sveltebase/tests/LoginTest.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Test login system', async ({ page }) => {
-  await page.goto('https://sveltebase.vercel.app/');
+  await page.goto('http://localhost:4173/');
   await page.getByRole('link', { name: 'Login' }).click();
   await page.getByPlaceholder('username').click();
   await page.getByPlaceholder('username').fill('test');
@@ -12,4 +12,6 @@ test('Test login system', async ({ page }) => {
   await page.getByRole('banner', { name: 'Site Header' }).getByText('Signed in as test').click();
   await page.getByRole('banner', { name: 'Site Header' }).getByRole('button', { name: 'Sign out' }).click();
   await page.getByRole('link', { name: 'Home' }).click();
+  //await page.getByRole('heading', { name: 'Welcome to SvelteKit' }).click();
+
 });
