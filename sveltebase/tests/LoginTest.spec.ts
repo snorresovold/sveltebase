419af293cbf6852e022a1c058166b3130fde7b6b
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2023-02-24 12:33:05 +0100

    test updates

--- sveltebase/tests/LoginTest.spec.ts
+++ sveltebase/tests/LoginTest.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Test login system', async ({ page }) => {
-  await page.goto('http://localhost:4173/'); // the url of the playwright server
+  await page.goto('/'); // the url of the playwright server
   await page.getByRole('link', { name: 'Login' }).click();
   await page.getByPlaceholder('username').click();
   await page.getByPlaceholder('username').fill('test');
@@ -12,6 +12,4 @@ test('Test login system', async ({ page }) => {
   await page.getByRole('banner', { name: 'Site Header' }).getByText('Signed in as test').click();
   await page.getByRole('banner', { name: 'Site Header' }).getByRole('button', { name: 'Sign out' }).click();
   await page.getByRole('link', { name: 'Home' }).click();
-  //await page.getByRole('heading', { name: 'Welcome to SvelteKit' }).click();
-
 });
