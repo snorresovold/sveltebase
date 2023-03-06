91e66d1b6fa523f50c06560d369de03af481d679
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2023-03-06 11:14:01 +0100

    updates

--- sveltebase/tests/Button.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/experimental-ct-svelte';
-import Button from "$lib/Button.svelte";
-
-test('Test Button', async ({ mount }) => {
-  const component = await mount(Button);
-  await expect(component).toContainText('hello');
-});
